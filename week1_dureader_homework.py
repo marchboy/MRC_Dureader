095d43eca7c6498c7ca0b8a7219da3629049ecb9
marchboy <jjgdut@163.com>
2021-07-04 23:53:37 +0800

    mrc_week1

---
+++ week1_dureader_homework.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import json
+import jieba
+
+
+path = r'D:/Projects/MRC/week1_机器阅读理解发展及任务解析/DuReader/data/preprocessed/trainset/search.train.json'
+
+
+
